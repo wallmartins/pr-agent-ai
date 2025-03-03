eaf397d180fff27f8893b59582052d5f9501d0a8
wallmartins <wall_martins_@hotmail.com>
2025-03-03 17:00:40 -0300

    feat(ollama.service): create ollama service to integrate app with ollama server and create pr service to create pr description and pr on github

---
+++ src/ollama/ollama.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { OllamaService } from './ollama.service';
+
+@Controller('ollama')
+export class OllamaController {
+  constructor(private readonly ollamaService: OllamaService) {}
+}
